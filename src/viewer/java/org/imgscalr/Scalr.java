8b706e99a9e85c5f88413081493abb015f01d71a
Una Thompson <me@unascribed.com>
2023-09-13 10:35:19 -0700

    Make viewer compileable, fix license headers

--- src/viewer/java/org/imgscalr/Scalr.java
+++ src/viewer/java/org/imgscalr/Scalr.java
@@ -1,20 +1,3 @@
-/*
- * Visage - the beautiful Minecraft avatar rendering service
- * Copyright (C) 2015-2023  Una Thompson (unascribed)
- *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, only version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
- * General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 /**
  * Copyright 2011 The Buzz Media, LLC
  * 
